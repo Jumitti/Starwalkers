8171dc1e76fb7d3284308dc79704a409bc328d98
Jumitti <112354223+Jumitti@users.noreply.github.com>
2024-02-02 16:01:28 +0100

    (v2.04) Improvements of case_menu Add Half and Max button

--- keyboard.py
+++ keyboard.py
@@ -1,13 +1,20 @@
 from telepot.namedtuple import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, KeyboardButton
 
-
-def main_keyboard():
-    buttons = [
+main_buttons = [
             [KeyboardButton(text='🏪 Case Menu'), KeyboardButton(text='💸 Buy case'), KeyboardButton(text='🎁 Open case')],
             [KeyboardButton(text='🚀 Collection/Fleet'), KeyboardButton(text='🫱🏽‍🫲🏽 Sell ship'), KeyboardButton(text='💥 Fight !')],
             [KeyboardButton(text='⏪ Exit'), KeyboardButton(text='🔄️ Restart'), KeyboardButton(text='❔ Help')]
         ]
 
-    custom_keyboard = ReplyKeyboardMarkup(keyboard=buttons, resize_keyboard=True)
+case_menu_buttons = [[KeyboardButton(text='1'), KeyboardButton(text='Half'), KeyboardButton(text='Max')]]
 
+
+def main_keyboard():
+    custom_keyboard = ReplyKeyboardMarkup(keyboard=main_buttons, resize_keyboard=True)
+    return custom_keyboard
+
+
+def case_menu_keyboard():
+    buttons = case_menu_buttons + main_buttons
+    custom_keyboard = ReplyKeyboardMarkup(keyboard=buttons, resize_keyboard=True)
     return custom_keyboard
--- starwalkers_telebot.py
+++ starwalkers_telebot.py
@@ -3,6 +3,7 @@ import os
 import random
 import re
 import time
+import math
 
 import telepot
 from telepot.loop import MessageLoop
@@ -243,12 +244,12 @@ def branch_to_leaf(chat_id, command, branch, leaf, money, user_case, ship_list):
             if command in ['1', '/buy_case', '💸 Buy case']:  # Buy case
                 bot.sendMessage(chat_id,
                                 f"Money: {money}$ | Case(s): {user_case}\nBuy how many cases ? ({settings['cost_case']}$ per case)",
-                                reply_markup=KB.main_keyboard())
+                                reply_markup=KB.case_menu_keyboard())
                 tree_choice(chat_id, branch, 1)
 
             elif command in ['2', '/open_case', '🎁 Open case']:  # Open case
                 bot.sendMessage(chat_id, f"Money: {money}$ | Case(s): {user_case}\nOpen how many cases ?",
-                                reply_markup=KB.main_keyboard())
+                                reply_markup=KB.case_menu_keyboard())
                 tree_choice(chat_id, branch, 2)
         else:
             bot.sendMessage(chat_id, "What do you want to do with cases?\n1. /buy_case\n2. /open_case",
@@ -299,35 +300,54 @@ def branch_to_leaf(chat_id, command, branch, leaf, money, user_case, ship_list):
 def leaf_output(chat_id, command, branch, leaf, money, user_case, ship_list, enemy_list):
     settings = settings_file()
     if branch == 1 and leaf == 1:  # Buy case
-        if command.isdigit():
-            number = int(command)
-            if money >= settings['cost_case'] * number:
+        if command.isdigit() or command in ['Half', 'Max']:
+            if command.isdigit() and money >= settings['cost_case'] * int(command):
+                number = int(command)
+            elif command == 'Half' and money >= settings['cost_case']:
+                if money == settings['cost_case']:
+                    number = 1
+                else:
+                    number = math.floor((money/settings['cost_case'])/2)
+            elif command == 'Max' and money >= settings['cost_case']:
+                number = math.floor((money/settings['cost_case']))
+            else:
+                number = 0
+                bot.sendMessage(chat_id, "Not enough money, bro. Try to /sell_ship or /fight\n\n/exit buying case.",
+                                reply_markup=KB.case_menu_keyboard())
+
+            if number != 0:
                 money -= settings['cost_case'] * number
                 user_case += number
                 bot.sendMessage(chat_id,
                                 f"Money: {money}$ | Case(s): {user_case}\nThanks for buying {number} cases.\n\n/open_case to earn ship\n/exit buying case.",
-                                reply_markup=KB.main_keyboard())
+                                reply_markup=KB.case_menu_keyboard())
                 save_json(chat_id, money=money, user_case=user_case)
-            else:
-                bot.sendMessage(chat_id, "Not enough money, bro. Try to /sell_ship or /fight\n\n/exit buying case.",
-                                reply_markup=KB.main_keyboard())
         else:
-            bot.sendMessage(chat_id, "Please use number or /exit", reply_markup=KB.main_keyboard())
+            bot.sendMessage(chat_id, "Please use number or /exit", reply_markup=KB.case_menu_keyboard())
 
     elif branch == 1 and leaf == 2:  # Open case
-        if command.isdigit():
-            number = int(command)
+        if command.isdigit() or command in ['Half', 'Max']:
             if user_case >= 1 and len(ship_list) < settings['ship_fleet']:
+                if command.isdigit():
+                    number = int(command)
+                elif command == 'Half':
+                    if user_case == 1:
+                        number = 1
+                    else:
+                        number = math.floor(user_case / 2)
+                elif command == 'Max':
+                    number = user_case
+
                 if number + len(ship_list) > settings['ship_fleet']:
                     if number > user_case:
                         number = user_case
                         bot.sendMessage(chat_id,
-                                        f"Only {number} case(s) will be opened.", reply_markup=KB.main_keyboard())
+                                        f"Only {number} case(s) will be opened.", reply_markup=KB.case_menu_keyboard())
                     else:
                         number = settings['ship_fleet'] - len(ship_list)
                         bot.sendMessage(chat_id,
                                         f"Only {settings['ship_fleet']} ships is allowed. {number} case(s) will be opened.",
-                                        reply_markup=KB.main_keyboard())
+                                        reply_markup=KB.case_menu_keyboard())
 
                 for i in range(0, number):
                     user_case -= 1
@@ -343,17 +363,17 @@ def leaf_output(chat_id, command, branch, leaf, money, user_case, ship_list, ene
                                 f"Money: {money}$ | Case(s): {user_case}\nThanks for buying {number} ship(s)."
                                 f"\n\nSee your ship(s) in /collection"
                                 f"\n\n Earn money by /sell_ship or /fight"
-                                f"\n\n/exit opening case.", reply_markup=KB.main_keyboard())
+                                f"\n\n/exit opening case.", reply_markup=KB.case_menu_keyboard())
 
             elif len(ship_list) == settings['ship_fleet']:
                 bot.sendMessage(chat_id,
                                 "You have too many ships. /sell_ship and you can open cases again.\n\n/exit opening case.",
-                                reply_markup=KB.main_keyboard())
+                                reply_markup=KB.case_menu_keyboard())
             elif user_case <= 0:
                 bot.sendMessage(chat_id,
-                                "You don't have case. /buy_case\n\n/exit opening case.", reply_markup=KB.main_keyboard())
+                                "You don't have case. /buy_case\n\n/exit opening case.", reply_markup=KB.case_menu_keyboard())
         else:
-            bot.sendMessage(chat_id, "Please use number or /exit", reply_markup=KB.main_keyboard())
+            bot.sendMessage(chat_id, "Please use number or /exit", reply_markup=KB.case_menu_keyboard())
 
     elif branch == 2 and leaf == 1:  # Selling ship
         if command.isdigit():
